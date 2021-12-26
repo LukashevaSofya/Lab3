08f04a281daaed3630a6a32fa9c32010a9124467
LukashevaSofya <92695517+LukashevaSofya@users.noreply.github.com>
2021-12-26 22:47:16 +0300

    Update индивидуалка1.py

--- индивидуалка1.py
+++ индивидуалка1.py
@@ -1,15 +1,23 @@
-
 from random import randint
-a = [[randint(1, 10) for i in range(9)],
-     [randint(1, 10) for i in range(9)],
-     [randint(1, 10) for i in range(9)]]
-b = [[randint(1, 10) for i in range(9)],
-     [randint(1, 10) for i in range(9)],
-     [randint(1, 10) for i in range(9)]]
+a=[[randint(1, 10) for i in range(9)],
+    [randint(1, 10) for i in range(9)],
+    [randint(1, 10) for i in range(9)],
+    [randint(1, 10) for i in range(9)],
+    [randint(1, 10) for i in range(9)],
+    [randint(1, 10) for i in range(9)],
+    [randint(1, 10) for i in range(9)],
+    [randint(1, 10) for i in range(9)],
+    [randint(1, 10) for i in range(9)]]
+print(a)
 
-for i in range(9):
-    s=0
-    for j in range(9):
-        s=s+a[i,j]+a[j,i]
-        a[i,i]=s-a[i,i]
-print(a,b) 
+sum1=0
+
+for i in range(0, 10):
+    for j in range(0, 10):
+        if j+1==10-i:
+            for k in range(0, 10):
+                sum1+=a #[k][j]
+    for k1 in range(0, 10):
+        sum1+=a #[i][k1]
+        a[i][j]=sum1-a[i][j]
+print(a)
